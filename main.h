cb86b6053f591b5d4177b499d8c9f0e90c469e7b
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-16 05:15:43 +0000

    Add changes to main.h

--- main.h
+++ main.h
@@ -21,7 +21,8 @@ typedef struct formatter
 /* MAIN FUNCTIONS */
 int _printf(const char *format, ...);
 
-int get_matching_func(const char *format, formatter_t format_list[], va_list args);
+int get_matching_func(const char *format, formatter_t format_list[],
+						va_list args);
 int print_char(va_list args);
 int print_str(va_list args);
 int print_percent(va_list args);
@@ -42,5 +43,31 @@ int print_reversed(va_list args);
 char *reverse_str(char *s);
 void write_base(char *str);
 unsigned int base_len(unsigned int num, unsigned int base);
+
+/*
+ * int _printf(const char *format, ...);
+ * int print_char(va_list arg);
+ * int print_string(va_list arg);
+ * int print_percent(va_list arg);
+ * int print_int_or_dec(va_list arg);
+ * int print_binary(va_list arg);
+ * int format_handler(const char *fmt, int *indx, va_list op_args);
+ * char *its(int num);
+ * char *_binary(int value);
+ */
+
+/*
+ * struct fmt_func - Struct fmt_funct
+ *
+ * @fmt_char: format specifier
+ * @func: pointer to function that handles a particular
+ * specifier
+ *
+ * typedef struct fmt_func
+ *{
+ *	char fmt_char;
+ *	int (*func)(va_list);
+ *} fmt_func_t;
+ */
 
 #endif /*MAIN_H*/
