8fe5d0a10b83944714cde104b7c75ee7e89adfd4
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-16 01:40:30 +0000

    Add changes to typedef name

--- main.h
+++ main.h
@@ -14,14 +14,14 @@ char *_binary(int value);
  * struct converter - defines a struct data type for symbols and functions
  * @symb: operator
  * @func: function handling the operator*/
-struct converter {
+struct formatter {
 	char *symb;
 	int (*func)(va_list);
 }
-typedef struct converter converter_t;
+typedef struct formatter formatter_t;
 
 /* --- FUNCTIONS --- */
-int handle_parsing(char *format, converter_t format_list[], va_list args);
+int handle_parsing(char *format, formatter_t format_list[], va_list args);
 
 int _printf(const char *format, ...);
 
