af2da52bf174831554f38c1c4e5d9df537d8b5b9
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-16 12:40:13 +0000

    Complete codebase rollback

--- format_handler.c
+++ format_handler.c
@@ -14,14 +14,10 @@ int format_handler(const char *fmt, int *indx, va_list op_args)
 	int i;
 
 	fmt_func_t arr[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'%', print_percent},
-		{'d', print_int_or_dec},
-		{'i', print_int_or_dec},
-		{'b', print_binary},
-		{'\0', NULL}};
-
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'d', print_int_or_dec}, {'i', print_int_or_dec}, {'b', print_binary},
+		{'\0', NULL}
+	};
 	for (i = 0; arr[i].fmt_char != '\0'; i++)
 	{
 		if (arr[i].fmt_char == fmt[*indx])
@@ -32,3 +28,4 @@ int format_handler(const char *fmt, int *indx, va_list op_args)
 			return (-1);
 	return (-1);
 }
+
