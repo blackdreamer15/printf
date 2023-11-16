11c8920271fd1d6977178674413f0b626d2cc9ac
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-16 03:33:47 +0000

    Add changes to _printf.c

--- _printf.c
+++ _printf.c
@@ -10,49 +10,27 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int chars_printed = 0;
-	converter_t format_list[] = {
+	formatter_t format_list[] = {
 		{'c', print_char},
 		{'s', print_str},
 		{'%', print_percent},
 		{'d', print_int},
 		{'i', print_int},
-		{'b', print_bin},
-                {'u', unsigned_int},
-                {'x', print_hex},
-                {'X', print_hex},
-		{NULL, NULL}
-	};
+		{'b', print_binary},
+		{'u', print_num},
+		{'o', print_octal},
+		{'x', print_hex},
+		{'X', print_HEX},
+		{'\0', NULL}};
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-
 	chars_printed = handle_parsing(format, format_list, args);
-			else if (*format == '%')
-				chars_printed += write(1, format, 1);
-			else if (*format == 'd' || *format == 'i')
-			{
-				char *c = its(va_arg(args, int));
-
-				if (c != NULL)
-				{
-					chars_printed += write(1, c, strlen(c));
-					free(c);
-				}
-			}
-			else if (*format == 'b')
-			{
-				char *bit = _binary(va_arg(args, int));
-				if (bit != NULL)
-				{
-					chars_printed += write(1, bit, strlen(bit));
-					free(bit);
-				}
 
 	va_end(args);
 
 	return (chars_printed);
 }
-
