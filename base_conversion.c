1928441395310c4442a3dcbc3125267354e20618
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-16 03:28:18 +0000

    Upd: Add a function to convert from decimal to hexadecimal

--- base_conversion.c
+++ base_conversion.c
@@ -143,3 +143,52 @@ int print_hex(va_list args)
 
 	return (len);
 }
+
+/**
+ * print_HEX - converts from decimal to hexadecimal
+ * @args: list of arguments
+ * Return: length of number printed.
+ */
+int print_HEX(va_list args)
+{
+	unsigned int num;
+	int i, len;
+	char *str, *rev_str;
+
+	num = var(args, unsigned int);
+
+	if (num == 0)
+		return (write(1, '0', 1));
+	if (num < 1)
+		return (-1);
+
+	len = base_len(num, 16);
+	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
+		return (-1);
+
+	for (i = 0; num > 0; i++)
+	{
+		if ((num % 16) < 10)
+			str[i] = (num % 16) + '0';
+		else
+			str[i] = (num % 16) + '7';
+		num /= 16;
+	}
+
+	rev_str = reverse_str(str);
+
+	if (rev_str == NULL)
+	{
+		free(str);
+		return (-1);
+	}
+
+	write_base(rev_str);
+
+	free(str);
+	free(rev_str);
+
+	return (len);
+}
