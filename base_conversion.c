a96b662e97dbe668aed6332e00746ba39638d501
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-16 03:26:37 +0000

    Upd: Add a function to convert from decimal to octal

--- base_conversion.c
+++ base_conversion.c
@@ -48,3 +48,49 @@ int print_binary(va_list args)
 
 	return (len);
 }
+
+/**
+ * print_octal - converts from decimal to octal
+ * @args: list of arguments
+ * Return: length of number printed.
+ */
+int print_octal(va_list args)
+{
+	unsigned int num;
+	int i, len;
+	char *str, *rev_str;
+
+	num = var(args, unsigned int);
+
+	if (num == 0)
+		return (write(1, '0', 1));
+	if (num < 1)
+		return (-1);
+
+	len = base_len(num, 8);
+	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
+		return (-1);
+
+	for (i = 0; num > 0; i++)
+	{
+		str[i] = (num % 8) + '0';
+		num /= 8;
+	}
+
+	rev_str = reverse_str(str);
+
+	if (rev_str == NULL)
+	{
+		free(str);
+		return (-1);
+	}
+
+	write_base(rev_str);
+
+	free(str);
+	free(rev_str);
+
+	return (len);
+}
