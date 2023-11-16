a971fa7df50426f0860c55953191296551d3bbc0
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-16 14:24:25 +0000

    Update - Clean up project

--- base_support_func.c
+++ base_support_func.c
@@ -11,7 +11,7 @@ char *its(int num)
 	int num_size;
 	char *num_arr;
 
-	num_size =  snprintf(NULL, 0, "%d", num);
+	num_size = snprintf(NULL, 0, "%d", num);
 	num_arr = malloc(num_size + 1);
 	if (!num_arr)
 		return (NULL);
@@ -26,7 +26,7 @@ char *its(int num)
  * Return: pointer to dynamically
  * allocated array
  */
-char *_binary(int value)
+char *_binary(unsigned int value)
 {
 	int i;
 	char *arr = malloc(8);
@@ -40,6 +40,32 @@ char *_binary(int value)
 
 		arr[i] = bit + '0';
 		value >>= 1;
+	}
+	return (arr);
+}
+
+/**
+ * _hex - converts number to hexadecimal
+ * @value: value to be converted
+ * Return: pointer to dynamically allocated array
+ */
+char *_hex(unsigned int value)
+{
+	int i;
+	char *arr = malloc(8);
+
+	if (!arr)
+		return (NULL);
+	arr[7] = '\0';
+	for (i = 6; i >= 0; i--)
+	{
+		int bit = value & 15;
+
+		if (bit < 10)
+			arr[i] = bit + '0';
+		else
+			arr[i] = bit + 'W';
+		value >>= 4;
 	}
 	return (arr);
 }
