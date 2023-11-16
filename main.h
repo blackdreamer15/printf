80a1056a50c575c34700b0079e12e3080c9103e9
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-16 05:18:43 +0000

    Add changes to main.h

--- main.h
+++ main.h
@@ -44,30 +44,28 @@ char *reverse_str(char *s);
 void write_base(char *str);
 unsigned int base_len(unsigned int num, unsigned int base);
 
-/*
- * int _printf(const char *format, ...);
- * int print_char(va_list arg);
- * int print_string(va_list arg);
- * int print_percent(va_list arg);
- * int print_int_or_dec(va_list arg);
- * int print_binary(va_list arg);
- * int format_handler(const char *fmt, int *indx, va_list op_args);
- * char *its(int num);
- * char *_binary(int value);
- */
+/*FROM GITHUB*/
+int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_string(va_list arg);
+int print_percent(va_list arg);
+int print_int_or_dec(va_list arg);
+int print_binary(va_list arg);
+int format_handler(const char *fmt, int *indx, va_list op_args);
+char *its(int num);
+char *_binary(int value);
 
-/*
+
+/**
  * struct fmt_func - Struct fmt_funct
- *
  * @fmt_char: format specifier
- * @func: pointer to function that handles a particular
- * specifier
- *
- * typedef struct fmt_func
- *{
- *	char fmt_char;
- *	int (*func)(va_list);
- *} fmt_func_t;
+ * @func: pointer to function that handles a particular specifier
  */
+typedef struct fmt_func
+{
+	char fmt_char;
+	int (*func)(va_list);
+} fmt_func_t;
+
 
 #endif /*MAIN_H*/
