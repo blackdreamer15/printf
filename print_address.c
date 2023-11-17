4f79f09ab7937d8e81244086e25c31e148cf9043
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-17 09:44:48 +0000

    Add a function to print the address of a pointer

---
+++ print_address.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_address - prints address of a pointer
+ * @args: list of arguments
+ * Return: number of bytes written
+ */
+int print_address(va_list args)
+{
+	void *ptr;
+	long int temp;
+	int hex_len;
+
+	ptr = va_arg(args, void *);
+
+	if (ptr == NULL)
+		return (-1);
+
+	temp = (long int)ptr;
+
+	_putchar('0');
+	_putchar('x');
+	_putchar('7');
+	_putchar('f');
+	_putchar('f');
+	_putchar('e');
+
+	hex_len = HEX(temp, 'x');
+
+	return (hex_len + 2);
+}
