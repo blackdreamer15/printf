e95e9b43346b3f169f1f5a5abe6f1e141af3ba36
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-17 08:19:50 +0000

    Group char related format function in one file

---
+++ format_func_char.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_char - prints characters
+ * @args: list of optional arguments
+ *
+ * Return: number of bytes written
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_percent - prints percent sign
+ * @args: list of optional arguments
+ *
+ * Return: number of bytes written
+ */
+int print_percent(__attribute__((unused)) va_list args)
+{
+	return (write(1, "%%", 1));
+}
