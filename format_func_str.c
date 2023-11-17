4ffbafce1eee42d06b8f5035c744a87bc02a67dc
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-17 09:44:29 +0000

    Upd: Add function implementing rot13

--- format_func_str.c
+++ format_func_str.c
@@ -72,7 +72,7 @@ int print_x_string(va_list args)
 				write(1, "0", 1);
 				len++;
 			}
-			len += HEX(temp);
+			len += HEX(temp, 'x');
 		}
 		else
 		{
@@ -81,5 +81,38 @@ int print_x_string(va_list args)
 		}
 	}
 
+	return (len);
+}
+
+/**
+ * print_rot13 - encodes a string using rot13
+ * @args: list of arguments
+ * Return: number of arguments printed
+ */
+int print_rot13(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int i, j, len = 0;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	if (str == NULL)
+		return (-1);
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				write(1, &rot13[j], 1);
+				break;
+			}
+		}
+		if (j == 52)
+			write(1, &str[i], 1);
+	}
 	return (len);
 }
