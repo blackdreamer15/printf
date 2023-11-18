9370f52f608bb79f6a72cbbd0df6bdb8349f61ee
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-18 04:44:15 +0000

    Testing new version of the codebase

--- _printf.c
+++ _printf.c
@@ -1,35 +1,55 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to format
- * @format: format string
- *
- * Return: number of bytes written
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int i, _printed, characters_printed = 0;
-
+	int i, printed = 0, chars_printed = 0;
+	int flags;
+	int width;
+	int precision;
+	int size;
+	int buffer_len = 0;
 	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
 
 	va_start(list, format);
-	for (i = 0; format[i] != '\0'; i++)
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			write(1, &format[i], 1);
-			characters_printed++;
+			buffer[buffer_len++] = format[i];
+			if (buffer_len == BUFF_SIZE)
+				print_buffer(buffer, &buffer_len);
+			/* write(1, &format[i], 1);*/
+			chars_printed++;
 		}
 		else
 		{
-			i++;
-			_printed = format_func_finder(format, &i, list);
-
-			if (_printed == -1)
+			print_buffer(buffer, &buffer_len);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+								   flags, width, precision, size);
+			if (printed == -1)
 				return (-1);
-			characters_printed += _printed;
+			chars_printed += printed;
 		}
 	}
+
+	print_buffer(buffer, &buffer_len);
+
 	va_end(list);
-	return (characters_printed);
+
+	return (chars_printed);
 }
