cb6001e6525fbff1dae1a65b4a13caf4b4573326
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-11-19 20:10:42 +0000

    Update - Add major changes

--- format_func_finder.c
+++ format_func_finder.c
@@ -7,6 +7,7 @@
  */
 int (*func_finder(const char *fmt, int index))(va_list, char *, unsigned int)
 {
+	int i = 0, j = 0, first_index = index;
 	fmt_func_t arr[] = {
 		{"c", print_char},
 		{"s", print_str},
@@ -61,9 +62,7 @@ int (*func_finder(const char *fmt, int index))(va_list, char *, unsigned int)
 		{" %", print_percent},
 		{NULL, NULL},
 	};
-	int i = 0, j = 0, first_index;
 
-	first_index = index;
 	while (arr[i].fmt_char)
 	{
 		if (fmt[index] == arr[i].fmt_char[j])
@@ -74,11 +73,85 @@ int (*func_finder(const char *fmt, int index))(va_list, char *, unsigned int)
 				break;
 		}
 		else
-		{
-			j = 0;
-			i++;
-			index = first_index;
-		}
+			j = 0, i++, index = first_index;
 	}
 	return (arr[i].func);
+}
+
+/**
+ * fmt_specifier_counter - returns the amount of identifiers.
+ * @s: argument indentifier
+ * @index: index of argument identifier.
+ * Return: amount of identifiers.
+ */
+int fmt_specifier_counter(const char *fmt, int index)
+{
+	int i = 0, j = 0, first_index = index;
+	fmt_func_t arr[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_int},
+		{"d", print_int},
+		{"b", print_binary},
+		{"u", print_unsigned_int},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_upper_hex},
+		{"S", print_x_string},
+		{"p", print_address},
+		{"li", print_long_int},
+		{"ld", print_long_int},
+		{"lu", print_long_unsigned_int},
+		{"lo", print_long_octal},
+		{"lx", print_long_hex},
+		{"lX", print_long_upper_hex},
+		{"hi", print_short_int},
+		{"hd", print_short_int},
+		{"hu", print_short_unsigned_int},
+		{"ho", print_short_octal},
+		{"hx", print_short_hex},
+		{"hX", print_short_upper_hex},
+		{"#o", print_oct_with_leading_zero},
+		{"#x", print_hex_with_leading_zero},
+		{"#X", print_num_upper_hex},
+		{"#i", print_int},
+		{"#d", print_int},
+		{"#u", print_unsigned_int},
+		{"+i", print_int_with_symb},
+		{"+d", print_int_with_symb},
+		{"+u", print_unsigned_int},
+		{"+o", print_octal},
+		{"+x", print_hex},
+		{"+X", print_upper_hex},
+		{" i", print_int_with_space},
+		{" d", print_int_with_space},
+		{" u", print_unsigned_int},
+		{" o", print_octal},
+		{" x", print_hex},
+		{" X", print_upper_hex},
+		{"R", print_rot13},
+		{"r", print_rev_string},
+		{"%", print_percent},
+		{"l", print_percent},
+		{"h", print_percent},
+		{" +i", print_int_with_symb},
+		{" +d", print_int_with_symb},
+		{"+ i", print_int_with_symb},
+		{"+ d", print_int_with_symb},
+		{" %", print_percent},
+		{NULL, NULL}};
+
+	while (arr[i].fmt_char)
+	{
+		if (fmt[index] == arr[i].fmt_char[j])
+		{
+			if (arr[i].fmt_char[j + 1] != '\0')
+				index++, j++;
+			else
+				break;
+		}
+		else
+			j = 0, i++, index = first_index;
+	}
+	return (j);
 }
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
 
 /**
@@ -16,53 +17,68 @@
 typedef struct fmt_func
 {
 	char *fmt_char;
-	int (*f)(va_list, char *, unsigned int);
+	int (*func)(va_list, char *, unsigned int);
 } fmt_func_t;
 
 /* MAIN FUNCTION */
 int _printf(const char *format, ...);
 
-/* FORMAT FUNCTIONS */
-int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
-int ev_print_func(const char *s, int index);
+/* BUFFER HANDLING FUNCTIONS */
+unsigned int handle_buffer(char *buffer, char c, unsigned int buffer_len);
+int print_buffer(char *buffer, unsigned int buffer_len);
 
-int print_chr(va_list args, char *buffer, unsigned int buffer_len);
-int print_str(va_list args, char *buffer, unsigned int buffer_len);
-int print_prg(va_list __attribute__((unused)), char *, unsigned int);
-int print_int(va_list args, char *buffer, unsigned int buffer_len);
+/* BASE CONVERSIONS */
+char *binary_converter(char *binary_val, long int num, int is_neg, int limit);
+int print_binary(va_list args, char *buffer, unsigned int buffer_len);
 
-int print_bnr(va_list args, char *buffer, unsigned int buffer_len);
-int print_unt(va_list args, char *buffer, unsigned int buffer_len);
+char *octal_converter(char *bin_val, char *oct_val);
+char *short_octal_converter(char *bin_val, char *oct_val);
+char *long_octal_converter(char *bin_val, char *oct_val);
+int print_octal(va_list args, char *buffer, unsigned int buffer_len);
+int print_short_octal(va_list args, char *buffer, unsigned int buffer_len);
+int print_long_octal(va_list args, char *buffer,
+unsigned int buffer_len);
+int print_oct_with_leading_zero(va_list args, char *buffer,
+unsigned int buffer_len);
+
+char *hex_converter(char *bin_val, char *hex_val, int is_upper, int limit);
 int print_hex(va_list args, char *buffer, unsigned int buffer_len);
-int print_upx(va_list args, char *buffer, unsigned int buffer_len);
-int print_oct(va_list args, char *buffer, unsigned int buffer_len);
+int print_short_hex(va_list args, char *buffer, unsigned int buffer_len);
+int print_long_hex(va_list args, char *buffer, unsigned int buffer_len);
+int print_upper_hex(va_list args, char *buffer, unsigned int buffer_len);
+int print_num_upper_hex(va_list args, char *buffer, unsigned int buffer_len);
+int print_short_upper_hex(va_list args, char *buffer, unsigned int buffer_len);
+int print_long_upper_hex(va_list args, char *buffer,
+unsigned int buffer_len);
+int print_hex_with_leading_zero(va_list args, char *buffer,
+unsigned int buffer_len);
 
-int print_rot(va_list args, char *buffer, unsigned int buffer_len);
-int print_rev(va_list args, char *buffer, unsigned int buffer_len);
+/* FORMAT FUNCTION SELECTOR */
+int (*func_finder(const char *fmt, int index))(va_list, char *, unsigned int);
+int fmt_specifier_counter(const char *fmt, int index);
 
-int print_usr(va_list args, char *buffer, unsigned int buffer_len);
-int print_add(va_list args, char *buffer, unsigned int buffer_len);
-int prinlint(va_list args, char *buffer, unsigned int buffer_len);
-int prinlunt(va_list args, char *buffer, unsigned int buffer_len);
-int prinloct(va_list args, char *buffer, unsigned int buffer_len);
-int prinlhex(va_list args, char *buffer, unsigned int buffer_len);
-int prinlupx(va_list args, char *buffer, unsigned int buffer_len);
-int prinhint(va_list args, char *buffer, unsigned int buffer_len);
-int prinhunt(va_list args, char *buffer, unsigned int buffer_len);
-int prinhoct(va_list args, char *buffer, unsigned int buffer_len);
-int prinhhex(va_list args, char *buffer, unsigned int buffer_len);
-int prinhupx(va_list args, char *buffer, unsigned int buffer_len);
-int prinpint(va_list args, char *buffer, unsigned int buffer_len);
-int prinnoct(va_list args, char *buffer, unsigned int buffer_len);
-int prinnhex(va_list args, char *buffer, unsigned int buffer_len);
-int prinnupx(va_list args, char *buffer, unsigned int buffer_len);
-int prinsint(va_list args, char *buffer, unsigned int buffer_len);
-unsigned int handl_buffer(char *buffer, char c, unsigned int buffer_len);
-int print_buffer(char *buffer, unsigned int nbuffer);
-char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
-char *fill_oct_array(char *bnr, char *oct);
-char *fill_long_oct_array(char *bnr, char *oct);
-char *fill_short_oct_array(char *bnr, char *oct);
-char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+/* FORMAT FUNCTIONS */
+int print_char(va_list args, char *buffer, unsigned int buffer_len);
+int print_percent(__attribute__((unused)) va_list, char *, unsigned int);
+
+int print_int(va_list args, char *buffer, unsigned int buffer_len);
+int print_short_int(va_list args, char *buffer, unsigned int buffer_len);
+int print_long_int(va_list args, char *buffer, unsigned int buffer_len);
+int print_int_with_symb(va_list args, char *buffer, unsigned int buffer_len);
+int print_int_with_space(va_list args, char *buffer, unsigned int buffer_len);
+
+int print_unsigned_int(va_list args, char *buffer,
+unsigned int buffer_len);
+int print_long_unsigned_int(va_list args, char *buffer,
+unsigned int buffer_len);
+int print_short_unsigned_int(va_list args, char *buffer,
+unsigned int buffer_len);
+
+int print_str(va_list args, char *buffer, unsigned int buffer_len);
+int print_rot13(va_list args, char *buffer, unsigned int buffer_len);
+int print_rev_string(va_list args, char *buffer, unsigned int buffer_len);
+int print_x_string(va_list args, char *buffer, unsigned int buffer_len);
+
+int print_address(va_list args, char *buffer, unsigned int buffer_len);
 
 #endif
